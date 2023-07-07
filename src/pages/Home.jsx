e565999be6c7fa23259e146df4801fb4502fb849
frankie <frankie.j.kim@polarisoffice.com>
2023-07-07 18:05:24 +0900

    젠킨스 테스트

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import Style from '../styles/App.module.scss';
 const Home = () => {
-  return <div className={Style.test}>Home입니다ㅋㅋ!!</div>;
+  return <div className={Style.test}>Home입니다!</div>;
 };
 
 export default Home;
