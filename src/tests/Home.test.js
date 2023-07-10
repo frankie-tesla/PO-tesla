142ace9014ac95ea972fe04146f70fbdb2f45153
frankie <frankie.j.kim@polarisoffice.com>
2023-07-10 17:51:14 +0900

    실패하는 테스트코드

--- src/tests/Home.test.js
+++ src/tests/Home.test.js
@@ -3,6 +3,6 @@ import Home from 'pages/Home';
 
 test('renders Home Component', () => {
   render(<Home />);
-  const Homeelement = screen.getByText(/Home/i);
+  const Homeelement = screen.getByText(/HI/i);
   expect(Homeelement).toBeInTheDocument();
 });
