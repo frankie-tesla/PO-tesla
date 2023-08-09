071986cad31eebedc0093b5db0ac2bbb8602483c
frankie <frankie.j.kim@polarisoffice.com>
2023-08-09 14:26:04 +0900

    환경변수 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import Style from '../styles/App.module.scss';
 const Home = () => {
   return (
-    <div className={Style.test}>Home입니다 {process.env.REACT_APP_URL}</div>
+    <div className={Style.test}>Home입니다 {process.env.REACT_APP_TEST}</div>
   );
 };
 
