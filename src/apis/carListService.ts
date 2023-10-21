4fd81a2b78cf81182360e9cc11df4dbae15ae205
frankie <frankie.j.kim@polarisoffice.com>
2023-10-21 18:27:53 +0900

    feat: 차량용 드라이브 API 정의 및 타입 추가

--- src/apis/carListService.ts
+++ src/apis/carListService.ts
@@ -1,25 +1,39 @@
 import request from "./core";
-import { DriveRequest, Login } from "./type";
-
+import { DriveListRes, BaseRequest, Favorite, ListData, Login, FileInfo, SearchFileReq } from "./type";
+import { AxiosResponse } from "axios";
 export default class CarService {
   baseUrl: string;
   constructor() {
-    this.baseUrl = "/1/drive/car";
+    this.baseUrl = "/2/drive";
   }
 
-  async getDriveList(data: DriveRequest) {
-    const response: unknown = await request.post(`${this.baseUrl}/list`, data);
-    return response;
+  async getDriveList(data: BaseRequest) {
+    const response: AxiosResponse<DriveListRes> = await request.post(`${this.baseUrl}/page/list`, data);
+    return response.data;
   }
 
-  async getShareList(data: DriveRequest) {
-    const response: unknown = await request.post(`${this.baseUrl}/cowork/list`, data);
-    return response;
+  async getShareList(data: Favorite) {
+    const response: AxiosResponse<{ workList: { fileInfo: FileInfo }[]; totalCount: number }> = await request.post(
+      "/2/cowork/page/list",
+      data
+    );
+    return response.data;
   }
 
-  async getFavoriteList(data: DriveRequest) {
-    const response: unknown = await request.post(`${this.baseUrl}/star/list`, data);
-    return response;
+  async getFavoriteList(data: Favorite) {
+    const response: AxiosResponse<{ list: ListData[]; totalCount: number }> = await request.post(
+      `${this.baseUrl}/star/page/list`,
+      data
+    );
+    return response.data;
+  }
+
+  async searchFile(data: SearchFileReq) {
+    const response: AxiosResponse<{ list: ListData[]; totalCount: number }> = await request.post(
+      `${this.baseUrl}/search/page/filename`,
+      data
+    );
+    return response.data;
   }
 
   async Login(data: Login) {
--- src/apis/type.ts
+++ src/apis/type.ts
@@ -1,14 +1,17 @@
-type BaseRequest = {
-  parentId?: string;
+export type BaseRequest = {
+  parentId?: string | null;
   fileType?: string;
   recursive?: boolean;
-  path?: string;
-  fileStatus: "NORMAL" | "HIDDEN" | "ALL";
+  path?: string | null;
+  fileStatus?: "NORMAL" | "HIDDEN" | "ALL";
+  keyword?: string;
+} & Favorite;
+
+export type Favorite = {
   page: number;
   count: number;
   sort: "TIME" | "NAME";
   desc: boolean;
-  keyword?: string;
 };
 type RequireAtLeastOne<T, Keys extends keyof T> = Pick<T, Exclude<keyof T, Keys>> &
   {
@@ -22,3 +25,82 @@ export type Login = {
   email: string;
   password: string;
 };
+
+type DirectoryInfo = {
+  fileId: string;
+  fileName: string;
+  lastRevision: number;
+  lastModified: number;
+  fileType: string;
+  parentId: null | number;
+  size: number;
+  hide: boolean;
+  path: string;
+  taskId: null | number;
+  lastModifiedRevision: number;
+  fileRevision: number;
+  pageCount: number;
+  editable: boolean;
+  referenceId: null | number;
+  shared: boolean;
+  starred: boolean;
+  starredTime: number;
+  weblinkCreated: boolean;
+  deletedTime: number;
+  lastAccessTime: number;
+  pinUp: boolean;
+  teamFile: boolean;
+  userid: string;
+};
+export type DriveListRes = {
+  list: ListData[];
+  directoryInfo: DirectoryInfo;
+  totalCount: number;
+};
+
+export type UserInfo = {
+  level: 1 | 4 | 8 | 9 | 12 | 13 | undefined;
+  fullName: string;
+  email: string;
+};
+export type AiUserInfo = {
+  userinfo: {
+    credit: number | null;
+    remainCredit: number;
+  };
+};
+
+export type ListData = {
+  fileId: string;
+  fileName: string;
+  lastModified: number;
+  fileType: string;
+  parentId: string;
+  size: number;
+  referenceId: null | number;
+};
+
+export type FileInfo = {
+  id: string;
+  name: string;
+  size: number;
+  lastModified: number;
+  countWebview: number;
+  countComments: number;
+  ownerId: string;
+  ownerName: string;
+  revision: number;
+  revisionFile: number;
+  path: string;
+  parentId: string;
+  fileType: string;
+};
+
+export type SearchFileReq = {
+  keyword: string;
+  parentId: string;
+  fileType: "FILE" | "DIR" | "ALL";
+  menuType: "DRIVE" | "SHARE" | "STAR";
+} & Favorite;
+
+export type DriveLocation = { fileId: string; fileName: string };
