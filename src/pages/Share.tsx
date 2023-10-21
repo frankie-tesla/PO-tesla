14397a8f1f9f921b1472c5709c8dc013ea164cd7
frankie <frankie.j.kim@polarisoffice.com>
2023-10-21 19:30:50 +0900

    feat: 공유문서 리스트 API 연동

--- src/pages/Share.tsx
+++ src/pages/Share.tsx
@@ -1,7 +1,63 @@
-import Nodoc from "../components/Nodoc";
-
+import { useState, useRef, useEffect } from "react";
+import FileListWrapper from "../components/lists/FileListWrapper.tsx";
+import useDriveQuery from "../hooks/useDriveQuery.tsx";
+import { Favorite } from "../apis/type";
+import { useDataListsContext } from "../context/DataListsContext.tsx";
 const Share = () => {
-  return <Nodoc type="공유 문서"></Nodoc>;
+  const ref = useRef<HTMLDivElement>(null);
+  const [page, setPage] = useState(1);
+  const postPerPage = Math.ceil((ref.current?.clientHeight || 500) / 50);
+  const {
+    actions: { setList }
+  } = useDataListsContext();
+  const { getShareList } = useDriveQuery();
+  const [driveRequestData, setDriveRequestData] = useState<Favorite>({
+    page: page,
+    count: postPerPage,
+    sort: "TIME",
+    desc: true
+  });
+
+  useEffect(() => {
+    setDriveRequestData({
+      ...driveRequestData,
+      page: page
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page]);
+
+  const { isLoading, data, isSuccess } = getShareList(driveRequestData);
+  const list = data?.workList.map((item) => {
+    const { id, name, lastModified, fileType, parentId, size } = item.fileInfo;
+    return {
+      fileId: id,
+      fileName: name,
+      lastModified,
+      fileType,
+      parentId,
+      size,
+      referenceId: null
+    };
+  });
+
+  useEffect(() => {
+    if (isSuccess) {
+      setList(list ?? []);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isSuccess]);
+  return (
+    <FileListWrapper
+      setDriveRequestData={setDriveRequestData}
+      driveRequestData={driveRequestData}
+      page={page}
+      setPage={setPage}
+      ref={ref}
+      totalPage={Math.ceil((data?.totalCount ?? 10) / postPerPage)}
+      isLoading={isLoading}
+      datas={list}
+    />
+  );
 };
 
 export default Share;
